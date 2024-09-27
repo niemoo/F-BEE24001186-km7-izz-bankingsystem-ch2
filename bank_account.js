0b44ab647c43c03c6933d1de66ee037ddcf45b7f
niemoo <izzanabdul123@gmail.com>
2024-09-28 00:08:45 +0700

    feat(withdraw): add withdraw method

--- bank_account.js
+++ bank_account.js
@@ -20,7 +20,22 @@ class BankAccount {
     });
   }
 
-  withdraw(amount) {}
+  withdraw(amount) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (isNaN(amount)) {
+          reject(new notNumberError('Input harus berupa angka.'));
+        } else if (amount < 0) {
+          reject(new negativeAmountError('Input tidak boleh kurang dari 0.'));
+        } else if (amount > 0 && amount <= this.accountBalance) {
+          this.accountBalance -= amount;
+          resolve('Saldo berhasil dikurangi.');
+        } else {
+          reject(new notEnoughBalanceError('Saldo tidak mencukupi.'));
+        }
+      }, 1000);
+    });
+  }
 }
 
 module.exports = { BankAccount };
