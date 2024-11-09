21fe9db5097786df2725340cfed43ddb6bb0d7f1
Izzan Abdul Aziz <112173718+niemoo@users.noreply.github.com>
2024-11-09 08:19:09 +0700

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const main = () => {
   const app = express();
   dotenv.config();
   const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   };
